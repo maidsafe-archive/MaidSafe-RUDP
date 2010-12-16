7ac74f15cc6086a7f9de775f249f480fc961be86
Mahmoud Moadeli <mmoadeli@maidsafe.net>
2010-12-16 15:29:35 +0000

    tcpTransport2 and tcpConnection2.
    git-svn-id: https://maidsafe-dht.googlecode.com/svn/branches/signals@626 16f77258-3af3-11de-be05-5f7a86268029

--- src/maidsafe/transport/tcpconnection2.h
+++ src/maidsafe/transport/tcpconnection2.h
@@ -1,6 +1,7 @@
 /* Copyright (c) 2010 maidsafe.net limited
 All rights reserved.
 
+
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
