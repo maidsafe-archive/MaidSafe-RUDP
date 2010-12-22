7d51a00dc32cbc6a12819af428c6377227374503
Steve Muecklisch <steve.muecklisch@maidsafe.net>
2010-12-22 15:14:52 +0000

    Revised kademlia::Contact, fixed typo in protobuf::Contact.
    git-svn-id: https://maidsafe-dht.googlecode.com/svn/branches/signals@640 16f77258-3af3-11de-be05-5f7a86268029

--- src/maidsafe/kademlia/contact.cc
+++ src/maidsafe/kademlia/contact.cc
@@ -25,112 +25,142 @@ TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include "maidsafe/kademlia/contact.h"
+
 #include <boost/lexical_cast.hpp>
 #include "maidsafe/base/utils.h"
-#include "maidsafe/kademlia/kademlia.pb.h"
-#include "maidsafe/kademlia/contact.h"
-#include "maidsafe/kademlia/nodeid.h"
 
 namespace kademlia {
 
 Contact::Contact()
     : node_id_(),
-      ep_(),
-      rv_ep_(),
-      local_eps_(),
-      last_seen_(base::GetEpochMilliseconds())
-      {}
+      endpoint_(),
+      rendezvous_endpoint_(),
+      local_endpoints_(),
+      num_failed_rpcs_(0),
+      last_seen_(base::GetEpochMilliseconds()),
+      prefer_local_(false) {}
+
+Contact::Contact(const Contact &other)
+    : node_id_(other.node_id_),
+      endpoint_(other.endpoint_),
+      rendezvous_endpoint_(other.rendezvous_endpoint_),
+      local_endpoints_(other.local_endpoints_),
+      num_failed_rpcs_(other.num_failed_rpcs_),
+      last_seen_(other.last_seen_),
+      prefer_local_(other.prefer_local_) {}
 
 Contact::Contact(const protobuf::Contact &contact)
-     :failed_rpc_(0),
-      local_eps_(),
-      rv_ep_(),
-      last_seen_(base::GetEpochMilliseconds()) {
-  if (!FromProtobuf(contact)) {
-   node_id_ = NodeId(kZeroId);
-   ep_();
-   rv_ep_();
-   local_eps_.clear();
-  }   
+    : node_id_(),
+      endpoint_(),
+      rendezvous_endpoint_(),
+      local_endpoints_(),
+      num_failed_rpcs_(0),
+      last_seen_(base::GetEpochMilliseconds()),
+      prefer_local_(false) {
+  FromProtobuf(contact);
 }
+
+Contact::Contact(const std::string &node_id,
+                 const transport::Endpoint ep)
+    : node_id_(node_id),
+      endpoint_(ep),
+      rendezvous_endpoint_(),
+      local_endpoints_(),
+      num_failed_rpcs_(0),
+      last_seen_(base::GetEpochMilliseconds()),
+      prefer_local_(false) {}
 
 bool Contact::FromProtobuf(const protobuf::Contact &contact) {
   if (!contact.IsInitialized())
     return false;
-  node_id_ = NodeId(contact.node_id());
-  ep_(contact.enpoint());
-  if (contact.has_rendezvous())
-    rv_ep_(contact.rendezvous());
   for (int i = 0; i < contact.local_ips_size(); ++i) {
-    Endpoint ep;
-    if (!ep.ip.ParseFromString(contact.local_ips(i)))
+    transport::Endpoint ep;
+    if (!ep.ip.from_string(contact.local_ips(i))) {
+      local_endpoints_.clear();
       return false;
+    }
     ep.port = contact.local_port();
-    local_eps_.push_back(ep);
+    local_endpoints_.push_back(ep);
+  }
+  node_id_ = NodeId(contact.node_id());
+  endpoint_.ip.from_string(contact.endpoint().ip());
+  endpoint_.port = contact.endpoint().port();
+  if (contact.has_rendezvous()) {
+    rendezvous_endpoint_.ip.from_string(contact.rendezvous().ip());
+    rendezvous_endpoint_.port = contact.rendezvous().port();
+  } else {
+    rendezvous_endpoint_ = transport::Endpoint();
   }
   return true;
 }
 
-protobuf::Contact& Contact::ToProtobuf() {
+protobuf::Contact Contact::ToProtobuf() const {
   protobuf::Contact contact;
+  boost::system::error_code ec;
   contact.set_node_id(node_id_.String());
-  contact.enpoint() = ep_;
-  contact.rendezvous() = rv_ep_;
-  std::list<Endpoint>::iterator it = local_eps_.begin();
-  for (; it != local_eps_.end(); ++it)
-    contact.add_local_ips((*it).ip.to_string());
-  contact.set_local_port((*it).port);
+  contact.mutable_endpoint()->set_ip(endpoint.ip.to_string(ec));
+  contact.mutable_endpoint()->set_port(endpoint.port);
+  if (rendezvous_endpoint_.port != 0) {
+    contact.mutable_rendezvous()->set_ip(rendezvous_endpoint_.ip.to_string(ec));
+    contact.mutable_rendezvous()->set_port(rendezvous_endpoint_.port);
+  }
+  for (std::list<transport::Endpoint>::iterator it = local_endpoints_.begin();
+       it != local_endpoints_.end(); ++it) {
+    contact.add_local_ips((*it).ip.to_string(ec));
+    contact.set_local_port((*it).port);
+  }
+  return contact;
 }
 
-Contact::Contact(const Contact &other)
-    : node_id_(other.node_id_), ep_(other.ep_),
-      failed_rpc_(other.failed_rpc_),
-      rv_ep_(other.rv_ep_),
-      last_seen_(other.last_seen_), local_eps_(other.local_eps_)
-      {}
+bool Contact::SetPreferredEndpoint(const transport::IP &ip) {
+  prefer_local_ = false;
+  if (endpoint_.ip == ip) {
+    return true;
+  } else {
+    std::list<transport::Endpoint>::iterator it = local_endpoints_.begin();
+    for (; it != local_endpoints_.end(); ++it) {
+      if (ip == (*it).ip) {
+        if (it != local_endpoints_.begin()) {
+          transport::Endpoint ep = (*it);
+          local_endpoints_.erase(it);
+          local_endpoints_.push_front(ep);
+        }
+        prefer_local_ = true;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+transport::Endpoint Contact::GetPreferredEndpoint() const {
+  if (prefer_local_ && !local_endpoints_.empty())
+    return local_endpoints_.front();
+  else
+    return endpoint_;
+}
 
 bool Contact::Equals(const Contact &other) const {
   if (node_id_ == other.node_id_)
     return (node_id_.String() != kClientId) ||
-           (ep_.ip == other.ep_.ip );
+           (endpoint_.ip == other.endpoint_.ip );
   return false;
 }
-
-Contact& Contact::operator=(const Contact &other) {
-  this->node_id_ = other.node_id_;
-  this->ep_ = other.ep_;
-  this->failed_rpc_ = other.failed_rpc_;
-  this->rv_ep_ = other.rv_ep_;
-  this->last_seen_ = other.last_seen_;
-  this->local_eps_ = other.local_eps_;
-  return *this;
-}
-
-bool Contact::SetPreferredEndpoint(transport::IP ip) {
-   prefer_local_ = false;
-   if (ep_.ip != ip) {
-     std::list<Endpoint>::iterator it = local_eps_.begin();
-     for (; it != local_eps_.end(); ++it) {
-       if ((ip == (*it).ip) && (it != local_eps_.begin())) {
-         Endpoint ep = (*it);
-         local_eps_.erase(it);
-         local_eps_.push_front(ep);
-         prefer_local_ = true;
-         break;
-       }
-     }
-   }      
- }
-
- Endpoint Contact::GetPreferredEndpoint() {
-   if (prefer_local_ && (local_eps_.size() != 0))
-     return local_eps_.front();
-   else
-     return ep_;
- }
  
 bool Contact::operator<(const Contact &rhs) const {
-  return this->node_id().String() < rhs.node_id().String();
+  return node_id().String() < rhs.node_id().String();
+}
+
+Contact Contact::operator=(const Contact &other) {
+  node_id_ = other.node_id_;
+  endpoint_ = other.endpoint_;
+  rendezvous_endpoint_ = other.rendezvous_endpoint_;
+  local_endpoints_ = other.local_endpoints_;
+  num_failed_rpcs_ = other.num_failed_rpcs_;
+  last_seen_ = other.last_seen_;
+  prefer_local_ = other.prefer_local_;
+  return *this;
 }
 
 }  // namespace kademlia
