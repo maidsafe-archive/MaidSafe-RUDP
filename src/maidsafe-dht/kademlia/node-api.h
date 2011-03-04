daccd0b83133ace1b8af0ca8b8de0ac85fd715c0
Dan Schmidt Valle <dan.schmidt@maidsafe.net>
2011-03-04 12:30:24 +0000

    Cleaning up some remaining warnings on linux. This should be warning free for linux

--- src/maidsafe-dht/kademlia/node-api.h
+++ src/maidsafe-dht/kademlia/node-api.h
@@ -45,10 +45,10 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "maidsafe-dht/kademlia/config.h"
 #include "maidsafe/common/version.h"
 
-//#if MAIDSAFE_DHT_VERSION < 25
-//#error This API is not compatible with the installed library.\
-//  Please update the maidsafe-dht library.
-//#endif
+// #if MAIDSAFE_DHT_VERSION < 25
+// #error This API is not compatible with the installed library.
+//   Please update the maidsafe-dht library.
+// #endif
 
 
 namespace maidsafe {
