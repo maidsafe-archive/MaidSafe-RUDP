e1f14a3e5911f7fed37bd1a2c02c68da64b91963
Jeremy Burns <jeremy.burns@maidsafe.net>
2011-04-04 13:08:44 +0100

    removal of header included for debugging [#11446591]

--- src/maidsafe-dht/kademlia/service.cc
+++ src/maidsafe-dht/kademlia/service.cc
@@ -44,7 +44,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "maidsafe/common/log.h"
 #include "maidsafe/common/crypto.h"
-#include "maidsafe/common/utils.h"
 
 namespace maidsafe {
 
