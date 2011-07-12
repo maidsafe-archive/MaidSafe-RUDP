76dc31760ef910579dd3a924dcdcdd690479512a
Mahmoud <mmoadeli@maidsafe.net>
2011-07-12 18:22:22 +0100

    implementation of random start stop in functional_node_test.cc

--- src/maidsafe/dht/tests/functional_kademlia/node_functional_test.cc
+++ src/maidsafe/dht/tests/functional_kademlia/node_functional_test.cc
@@ -31,8 +31,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <list>
 #include <set>
 #include <vector>
-#include "boost/asio/deadline_timer.hpp"
-#include <boost/numeric/conversion/cast.hpp>
 
 #include "boost/asio.hpp"
 #ifdef __MSVC__
@@ -44,6 +42,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #  pragma warning(pop)
 #endif
 #include "boost/lexical_cast.hpp"
+#include "boost/numeric/conversion/cast.hpp"
 
 #include "maidsafe/common/test.h"
 #include "maidsafe/common/crypto.h"
@@ -64,13 +63,17 @@ namespace kademlia {
 
 namespace test_node {
 
-const int kProbes = 4;
 const dht::transport::Port kStartingPort = 8000;
 const std::string kLocalIp = "127.0.0.1";
-const size_t kNetworkSize = 10;
-const size_t kMaxRestartCycles = 5;
+const unsigned int kMaxRestartCycles = 5;
 
-struct StartStop;
+struct SampleNodesStats {
+  explicit SampleNodesStats(int index)
+      : index(index),
+        restart_cycles(RandomUint32() % kMaxRestartCycles + 1) {}
+  size_t index;
+  size_t restart_cycles;
+};
 
 struct NodeContainer {
   NodeContainer()
@@ -96,6 +99,7 @@ struct NodeContainer {
         securifier(),
         transport(),
         message_handler(),
+        alternative_store(),
         node() {
     // set up ASIO service and thread pool
     work.reset(
@@ -104,6 +108,7 @@ struct NodeContainer {
     thread_group->create_thread(
         std::bind(static_cast<size_t(boost::asio::io_service::*)()>
             (&boost::asio::io_service::run), &asio_service));
+
     // set up data containers
     securifier.reset(new dht::Securifier(key_id, public_key, private_key));
     // set up and connect transport and message handler
@@ -114,9 +119,7 @@ struct NodeContainer {
             &dht::kademlia::MessageHandler::OnMessageReceived,
             message_handler.get(),
             _1, _2, _3, _4).track_foreign(message_handler));
-    
-//    timer.reset(new boost::asio::deadline_timer(asio_service));
-//    strand.reset(new boost::asio::io_service::strand(asio_service));
+
     // create actual node
     node.reset(new dht::kademlia::Node(asio_service, transport, message_handler,
                                        securifier, alternative_store,
@@ -124,8 +127,6 @@ struct NodeContainer {
                                        mean_refresh_interval));
   }
   dht::kademlia::AsioService asio_service;
-/*  std::shared_ptr<boost::asio::deadline_timer> timer;
-  std::shared_ptr<boost::asio::io_service::strand> strand;*/
   std::shared_ptr<boost::asio::io_service::work> work;
   std::shared_ptr<boost::thread_group> thread_group;
   std::shared_ptr<dht::Securifier> securifier;
@@ -133,19 +134,29 @@ struct NodeContainer {
   std::shared_ptr<dht::kademlia::MessageHandler> message_handler;
   dht::kademlia::AlternativeStorePtr alternative_store;
   std::shared_ptr<dht::kademlia::Node> node;
-  std::vector<std::shared_ptr<StartStop> > time_table;
 };
 
-struct StartStop {
-  StartStop() : start(0),
-                stop(0) {}
-  StartStop(size_t stop, size_t start) : stop(stop),
-                                         start(start) {}
-  size_t start;
-  size_t stop;
+struct TimerContainer {
+  TimerContainer()
+      : asio_service(),
+        work(),
+        thread_group(),
+        timer() {
+    work.reset(
+        new boost::asio::io_service::work(asio_service));
+    thread_group.reset(new boost::thread_group());
+    thread_group->create_thread(
+        std::bind(static_cast<size_t(boost::asio::io_service::*)()>
+            (&boost::asio::io_service::run), &asio_service));
+    timer.reset(new boost::asio::deadline_timer(asio_service));
+  }
+  dht::kademlia::AsioService asio_service;
+  std::shared_ptr<boost::asio::io_service::work> work;
+  std::shared_ptr<boost::thread_group> thread_group;
+  std::shared_ptr<boost::asio::deadline_timer> timer;
 };
 
-class FunctionalKNodeTest : public testing::Test {
+class FunctionalNodeTest : public testing::Test {
  public:
   void JoinCallback(size_t index,
                     int result,
@@ -155,8 +166,11 @@ class FunctionalKNodeTest : public testing::Test {
                     size_t *failed_nodes) {
     boost::mutex::scoped_lock lock(*mutex);
     if (result >= 0) {
-      if (index > 0 && index < kNetworkSize)
-        bootstrap_contacts_.push_back(nodes_[index]->node->contact());
+      if (index > 0 && index < network_size_) {
+        if ((std::find(bootstrap_contacts_.begin(), bootstrap_contacts_.end(),
+            nodes_[index]->node->contact())) == bootstrap_contacts_.end())
+          bootstrap_contacts_.push_back(nodes_[index]->node->contact());
+      }
       DLOG(INFO) << "Node " << (index + 1) << " joined.";
       ++(*joined_nodes);
     } else {
@@ -165,56 +179,72 @@ class FunctionalKNodeTest : public testing::Test {
     }
     cond_var->notify_one();
   }
-  
-  void HandleStart(const int index) {
-    size_t joined_nodes(0), failed_nodes(0);    
+
+  void HandleStart(const int index, size_t count) {
     {
-      boost::mutex::scoped_lock lock(mutex_);
-      if (++total_finished_ == total_restart_count_) {
-        cond_var_.notify_one();
+      boost::mutex::scoped_lock lock(mutex_start_);
+      size_t joined_nodes(0), failed_nodes(0);
+      dht::kademlia::JoinFunctor join_callback(std::bind(
+          &FunctionalNodeTest::JoinCallback, this, index, arg::_1, &mutex_,
+          &cond_var_, &joined_nodes, &failed_nodes));
+      EXPECT_FALSE(nodes_[index]->node->joined());
+      nodes_[index]->node->Join(
+         nodes_[index]->node->contact().node_id(),
+          bootstrap_contacts_, join_callback);
+      {
+        boost::mutex::scoped_lock lock(mutex_);
+        cond_var_.wait(lock);
+      }
+      EXPECT_TRUE(nodes_[index]->node->joined());
+      if (++total_finished_ == total_restart_) {
+        cond_var2_.notify_one();
         return;
       }
+      if (--count == 0)
+        return;
     }
-    dht::kademlia::JoinFunctor join_callback(std::bind(
-        &FunctionalKNodeTest::JoinCallback, this, 0, arg::_1, &join_mutex_,
-        &join_cond_var_, &joined_nodes, &failed_nodes));
-    EXPECT_FALSE(nodes_[sample_nodes_[index]]->node->joined());
-    timers[index]->expires_from_now(
-        boost::posix_time::millisec(nodes_[sample_nodes_[index]]->time_table.front()->stop));
-    nodes_[sample_nodes_[index]]->node->Join(
-        nodes_[sample_nodes_[index]]->node->contact().node_id(), 
-        bootstrap_contacts_, join_callback);
-    {
-      boost::mutex::scoped_lock lock(join_mutex_);
-      join_cond_var_.wait(lock);
-    }
-    EXPECT_FALSE(nodes_[sample_nodes_[index]]->node->joined());
-    timers[index]->async_wait(strands[index]->wrap(
-      std::bind(&FunctionalKNodeTest::HandleStop, this, index)));
-  }
-  
-  void HandleStop(const int index) {
-    ASSERT_FALSE(nodes_[sample_nodes_[index]]->time_table.empty());
-    EXPECT_TRUE(nodes_[index]->node->joined());
-    nodes_[sample_nodes_[index]]->node->Leave(NULL);
-    timers[index]->expires_from_now(
-        boost::posix_time::millisec(nodes_[sample_nodes_[index]]->time_table.front()->start));
-    nodes_[sample_nodes_[index]]->time_table.erase(nodes_[sample_nodes_[index]]->time_table.begin());
-    timers[index]->async_wait(
-      strands[index]->wrap(std::bind(&FunctionalKNodeTest::HandleStart, this,
-                                     index)));
-    EXPECT_FALSE(nodes_[index]->node->joined());
+    timers_[index]->timer->expires_from_now(
+        boost::posix_time::millisec(Stop()));
+    timers_[index]->timer->async_wait(std::bind(&FunctionalNodeTest::HandleStop,
+                                               this, index, count));
   }
 
+  void HandleStop(const int index, size_t count) {
+    {
+      boost::mutex::scoped_lock lock(mutex_stop_);
+      EXPECT_TRUE(nodes_[index]->node->joined());
+      nodes_[index]->node->Leave(NULL);
+      EXPECT_FALSE(nodes_[index]->node->joined());
+    }
+    timers_[index]->timer->expires_from_now(
+        boost::posix_time::millisec(Start()));
+    timers_[index]->timer->async_wait(
+        std::bind(&FunctionalNodeTest::HandleStart, this, index, count));
+  }
+
+
  protected:
-  FunctionalKNodeTest() : nodes_(),
-               kAlpha_(3),
-               kBeta_(2),
-               kReplicationFactor_(4),
-               total_finished_(0),
-               total_restart_count_(0),
-               kMeanRefreshInterval_(boost::posix_time::hours(1)),
-               bootstrap_contacts_() {}
+  FunctionalNodeTest()
+      : nodes_(),
+        mutex_(),
+        cond_var_(),
+        mutex2_(),
+        cond_var2_(),
+        mutex_start_(),
+        mutex_stop_(),
+        kAlpha_(3),
+        kBeta_(2),
+        kReplicationFactor_(4),
+        kMeanRefreshInterval_(boost::posix_time::hours(1)),
+        bootstrap_contacts_(),
+        timers_(),
+        sample_nodes_(),
+        network_size_(20),
+        total_finished_(0),
+        total_restart_(0) {
+     for (size_t index = 0; index < network_size_; ++index)
+       timers_.push_back(std::shared_ptr<TimerContainer>(new TimerContainer));
+  }
 
   virtual void SetUp() {
     size_t joined_nodes(0), failed_nodes(0);
@@ -225,8 +255,8 @@ class FunctionalKNodeTest : public testing::Test {
         node_id.String(), key_pair.public_key(), key_pair.private_key(), false,
         kReplicationFactor_, kAlpha_, kBeta_, kMeanRefreshInterval_)));
     dht::kademlia::JoinFunctor join_callback(std::bind(
-        &FunctionalKNodeTest::JoinCallback, this, 0, arg::_1, &mutex_, &cond_var_,
-        &joined_nodes, &failed_nodes));
+        &FunctionalNodeTest::JoinCallback, this, 0, arg::_1, &mutex_,
+        &cond_var_, &joined_nodes, &failed_nodes));
     dht::transport::Endpoint endpoint(kLocalIp, kStartingPort);
     std::vector<dht::transport::Endpoint> local_endpoints;
     local_endpoints.push_back(endpoint);
@@ -234,11 +264,10 @@ class FunctionalKNodeTest : public testing::Test {
                                    local_endpoints, endpoint, false, false,
                                    node_id.String(), key_pair.public_key(), "");
     bootstrap_contacts_.push_back(contact);
-    EXPECT_FALSE(nodes_[0]->node->joined());
     ASSERT_EQ(dht::transport::kSuccess,
               nodes_[0]->transport->StartListening(endpoint));
     nodes_[0]->node->Join(node_id, bootstrap_contacts_, join_callback);
-    for (size_t index = 1; index < kNetworkSize; ++index) {
+    for (size_t index = 1; index < network_size_; ++index) {
       crypto::RsaKeyPair tmp_key_pair;
       tmp_key_pair.GenerateKeys(4096);
       dht::kademlia::NodeId nodeid(dht::kademlia::NodeId::kRandomId);
@@ -265,27 +294,32 @@ class FunctionalKNodeTest : public testing::Test {
 
     {
       boost::mutex::scoped_lock lock(mutex_);
-      while (joined_nodes + failed_nodes < kNetworkSize)
+      while (joined_nodes + failed_nodes < network_size_)
         cond_var_.wait(lock);
     }
     EXPECT_EQ(0, failed_nodes);
-    for (int index = 0; index < nodes_.size(); ++index) {
-      std::shared_ptr<boost::asio::deadline_timer> timer(
-        new boost::asio::deadline_timer(nodes_[index]->asio_service));
-      timers.push_back(timer);
-      std::shared_ptr<boost::asio::io_service::strand> strand(
-           new boost::asio::io_service::strand(nodes_[index]->asio_service));
-      strands.push_back(strand);
+    // Selects the sample nodes and the restart frequencies
+    while (sample_nodes_.size() < network_size_ / 2) {
+      bool found = false;
+      size_t random = boost::numeric_cast<size_t>(
+                          RandomUint32() % network_size_);
+      for (size_t index = 0; index < sample_nodes_.size(); ++index) {
+        if (sample_nodes_[index]->index == random) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        sample_nodes_.push_back(
+            std::shared_ptr<SampleNodesStats>(new SampleNodesStats(random)));
+        total_restart_ += sample_nodes_.back()->restart_cycles;
+      }
     }
   }
 
   virtual void TearDown() {
     for (auto itr(nodes_.begin()); itr != nodes_.end(); ++itr) {
       if ((*itr)->node->joined()) {
-//      DLOG(INFO) << "Shutting down client " << (index + 1) << " of "
-//                 << nodes_.size() << " ...";
-//      if (std::find(nodes_left_.begin(), nodes_left_.end(),
-//          index) == nodes_left_.end()) {
         (*itr)->node->Leave(NULL);
         (*itr)->work.reset();
         (*itr)->asio_service.stop();
@@ -293,56 +327,60 @@ class FunctionalKNodeTest : public testing::Test {
         (*itr)->thread_group.reset();
       }
     }
+    for (auto itr(timers_.begin()); itr != timers_.end(); ++itr) {
+      (*itr)->work.reset();
+      (*itr)->asio_service.stop();
+      (*itr)->thread_group->join_all();
+    }
   }
 
+  size_t Stop() {
+    return boost::numeric_cast<size_t>((1000)*(2 +
+        (std::ceil(2.0/6.0 * std::exp((RandomUint32() % 100)/17.0)))));
+  }
+
+  size_t Start() {
+    return boost::numeric_cast<size_t>(
+        (1000)*(10 + static_cast<boost::uint64_t>(RandomUint32() % 20)));
+  }
+
+  std::vector<std::shared_ptr<NodeContainer> > nodes_;
   boost::mutex mutex_;
   boost::condition_variable cond_var_;
-  boost::mutex join_mutex_;
-  boost::condition_variable join_cond_var_;  
-  std::vector<std::shared_ptr<NodeContainer> > nodes_;
-  boost::thread_group thread_group_;
+  boost::mutex mutex2_;
+  boost::condition_variable cond_var2_;
+  boost::mutex mutex_start_;
+  boost::mutex mutex_stop_;
   const uint16_t kAlpha_;
   const uint16_t kBeta_;
   const uint16_t kReplicationFactor_;
   const boost::posix_time::time_duration kMeanRefreshInterval_;
   std::vector<dht::kademlia::Contact> bootstrap_contacts_;
-  std::vector<dht::kademlia::NodeId> nodes_id_;
+  std::vector<std::shared_ptr<TimerContainer> > timers_;
+  std::vector<std::shared_ptr<SampleNodesStats> > sample_nodes_;
+  size_t network_size_;
   size_t total_finished_;
-  size_t total_restart_count_;
-  std::vector<int> sample_nodes_;
-  std::vector<std::shared_ptr<boost::asio::deadline_timer> > timers;
-  std::vector<std::shared_ptr<boost::asio::io_service::strand> > strands;
+  size_t total_restart_;
 };
 
-TEST_F(FunctionalKNodeTest, FUNC_KAD_StartStopRandomNodes) {
-  int index = 0;
-  while (sample_nodes_.size() < kNetworkSize) {
-//    int random = RandomUint32() % kNetworkSize;
-//    if (std::find(sample_nodes_.begin(), sample_nodes_.end(), random) ==
-//        sample_nodes_.end()) 
-      sample_nodes_.push_back(index++);
+
+TEST_F(FunctionalNodeTest, FUNC_RandomStartStopNodes) {
+  for (auto it(sample_nodes_.begin()); it != sample_nodes_.end(); ++it) {
+    timers_[(*it)->index]->timer->expires_from_now(
+        boost::posix_time::millisec(Stop()));
+    timers_[(*it)->index]->timer->async_wait(
+        std::bind(&FunctionalNodeTest::HandleStop, this, (*it)->index,
+                                          (*it)->restart_cycles));
   }
-  for (int index = 0; index < sample_nodes_.size(); ++index) {
-    uint32_t random = RandomUint32();
-    size_t stop = boost::numeric_cast<size_t>((1000)*(2 +  boost::numeric_cast<size_t>(std::ceil(2.0/6.0 *
-          std::exp((random % 100)/17.0)))));
-    size_t start = boost::numeric_cast<size_t>((1000)*(stop + 10 + static_cast<boost::uint64_t>(random % 20)));
-    nodes_[index]->time_table.push_back(
-      std::shared_ptr<StartStop>(new StartStop(stop, start)));
-  }
-  for (int index = 0; index < sample_nodes_.size(); ++index) {
-    timers[index]->expires_from_now(boost::posix_time::millisec(
-        nodes_[index]->time_table.front()->stop));
-    timers[index]->async_wait(strands[index]->wrap(
-      std::bind(&FunctionalKNodeTest::HandleStop, this, index)));    
-  }
-  total_restart_count_ = 1;
   {
-    boost::mutex::scoped_lock lock(mutex_);
-    cond_var_.wait(lock);
+    boost::mutex::scoped_lock lock(mutex2_);
+    cond_var2_.wait(lock);
   }
 }
 
-}
-}
-}
+}  // namespace test_node
+
+}  // namespace kademlia
+
+}  // namespace maidsafe
+
