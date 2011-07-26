b23a9612f69b4b367533d005c9e95de79c2490da
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2011-07-26 21:53:47 +0100

    Updated version to 0.31.01 Changed to match Common v0.09.03

--- src/maidsafe/dht/transport/udp_transport.h
+++ src/maidsafe/dht/transport/udp_transport.h
@@ -40,7 +40,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "maidsafe/dht/transport/udp_request.h"
 #include "maidsafe/dht/version.h"
 
-#if MAIDSAFE_DHT_VERSION != 3100
+#if MAIDSAFE_DHT_VERSION != 3101
 #  error This API is not compatible with the installed library.\
     Please update the maidsafe-dht library.
 #endif
