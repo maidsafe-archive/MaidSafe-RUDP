52c9102072b31cf2d02debdfdac77275822cc63e
David Irvine <david.irvine@maidsafe.net>
2012-09-10 23:48:12 +0100

    typo

--- src/maidsafe/rudp/tests/managed_connections_test.cc
+++ src/maidsafe/rudp/tests/managed_connections_test.cc
@@ -995,7 +995,7 @@ TEST_F(ManagedConnectionsTest, BEH_API_BootstrapTimeout) {
   nodes_[0]->ResetData();
   result_of_send = kSuccess;
   result_arrived = false;
-  nodes_[0]->managed_connections()->Send(nodes.node_id(), "message04",
+  nodes_[0]->managed_connections()->Send(node_.node_id(), "message04",
                                          message_sent_functor);
   ASSERT_TRUE(wait_for_result());
   EXPECT_EQ(kInvalidConnection, result_of_send);
